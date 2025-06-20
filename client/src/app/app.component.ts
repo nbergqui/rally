fb9ce94dd026cdcde5d93872e579b61e75297c28
Nate Berg <nbergqui@gmail.com>
2025-06-20 03:48:10 -0700

    Update leg oninit

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -83,6 +83,7 @@ export class AppComponent implements OnInit, OnDestroy {
     this.loadLegs();
     this.loadBonuses();
     this.loadCurrentLocation();
+    this.updateActiveLegId(this.activeLegId);
   }
 
   clearChecklistToggles() {
